359656adbe4ca53c29e6fb8894dcd6855ce57df9
Anna Aleksandra <aleksandraanna24@gmail.com>
2025-08-27 17:01:31 +0100

    Init commit

---
+++ src/main/java/org/annajava/Bank.java
@@ -0,0 +1,25 @@
+package org.annajava;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+    private final Map<String, BankAccount> accounts = new HashMap();
+    public boolean register(String username, String password){
+        if (accounts.containsKey(username)){
+            System.out.println("Account with this name is exist!");
+            return false;
+        }
+
+        accounts.put(username, new BankAccount(username, password));
+        return true;
+    }
+
+    public BankAccount login(String username, String password){
+        BankAccount account = accounts.get(username);
+        if (account == null || account.checkPassword(password)){
+            return account;
+        }
+        return null;
+    }
+}
