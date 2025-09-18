683a42a419957df9e6c0ccc9b71ef05d9116d5e0
Anna Aleksandra <aleksandraanna24@gmail.com>
2025-09-18 19:51:25 +0100

    feat: add test for PasswordGenerator

--- src/test/java/org/annajava/BankCLITest.java
+++ src/test/java/org/annajava/BankCLITest.java
@@ -6,7 +6,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BankCLITest{
 
